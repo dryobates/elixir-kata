e4fec98d3ac2ed29332bd6b03d28998247bfa3bb
dryobates <jakub.stolarski@gmail.com>
2019-08-23 07:51:35 +0200

    B is not leap when not divisible by 4

--- leap_years/lib/leap_years.ex
+++ leap_years/lib/leap_years.ex
@@ -1,18 +1,5 @@
 defmodule LeapYears do
-  @moduledoc """
-  Documentation for LeapYears.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> LeapYears.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def is_leap(year) do
+    false
   end
 end
