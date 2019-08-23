ae852b74ecefe0cadc8419406ba6a6923ab5390e
dryobates <jakub.stolarski@gmail.com>
2019-08-23 07:52:11 +0200

    T is leap when divisible by 4

--- leap_years/test/leap_years_test.exs
+++ leap_years/test/leap_years_test.exs
@@ -5,4 +5,8 @@ defmodule LeapYearsTest do
   test "is not leap when not divisible by 4" do
     assert LeapYears.is_leap(2001) == false
   end
+
+  test "is leap when divisible by 4" do
+    assert LeapYears.is_leap(2004) == true
+  end
 end
