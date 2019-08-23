c66f7ffa319355eb0227e16fca42a3c03eb6d850
dryobates <jakub.stolarski@gmail.com>
2019-08-23 07:57:15 +0200

    T is not leap when divisible by 100

--- leap_years/test/leap_years_test.exs
+++ leap_years/test/leap_years_test.exs
@@ -9,4 +9,8 @@ defmodule LeapYearsTest do
   test "is leap when divisible by 4" do
     assert LeapYears.is_leap(2004) == true
   end
+
+  test "is not leap when divisible by 100" do
+    assert LeapYears.is_leap(1900) == false
+  end
 end
