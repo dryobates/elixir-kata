9d971cded911634cc930f40951f83b74d08ecc20
dryobates <jakub.stolarski@gmail.com>
2019-08-23 08:01:51 +0200

    B is leap when divisible by 400

--- leap_years/lib/leap_years.ex
+++ leap_years/lib/leap_years.ex
@@ -1,6 +1,7 @@
 defmodule LeapYears do
   def is_leap(year) do
     cond do
+      rem(year, 400) == 0 -> true
       rem(year, 100) == 0 -> false
       rem(year, 4) == 0 -> true
       true -> false
