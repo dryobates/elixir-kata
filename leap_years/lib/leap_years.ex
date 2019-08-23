392ea897a711e33c58294d18e232a7730c270efb
dryobates <jakub.stolarski@gmail.com>
2019-08-23 07:57:54 +0200

    B is not leap when divisible by 100

--- leap_years/lib/leap_years.ex
+++ leap_years/lib/leap_years.ex
@@ -1,9 +1,13 @@
 defmodule LeapYears do
   def is_leap(year) do
-    if rem(year, 4) == 0 do
-      true
-    else
+    if rem(year, 100) == 0 do
       false
+    else
+      if rem(year, 4) == 0 do
+        true
+      else
+        false
+      end
     end
   end
 end
