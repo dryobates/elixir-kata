5774c1e8ba69d9ae7aee966a9930dd341d283eac
dryobates <jakub.stolarski@gmail.com>
2019-08-23 08:10:38 +0200

    B raises not gregorian year before 1583

--- leap_years/lib/leap_years.ex
+++ leap_years/lib/leap_years.ex
@@ -1,5 +1,8 @@
 defmodule LeapYears do
   def is_leap(year) do
+    if year < 1583 do
+      raise NotGregorianYear
+    end
     cond do
       rem(year, 400) == 0 -> true
       rem(year, 100) == 0 -> false
